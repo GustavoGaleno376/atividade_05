c3fd7eda07993c941b506c85f2aaefe1f2e79a5e
GustavoGaleno376 <gustabrffmano@gmail.com>
2024-09-04 20:42:19 -0300

    atividade 05 tranformção do tempo

---
+++ atividade05.py
@@ -0,0 +1,4 @@
+# Escreva um programa que receba um valor em minutos e converta para segundos.
+minutos=int(input("digite em minutos"))
+segundos=(minutos*60)
+print(f"{minutos}minutos é {segundos} segundos" )
